01f77bb0ebc18f9e10a0ff11ad655313d3db2833
Mohammad Takbiri <mo.takbiri@gmail.com>
2021-10-18 06:09:52 +0000

    refactor: code style

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -1,7 +1,4 @@
+const baseUrl = import.meta.env.VITE_API_BASE;
 
-
-const baseUrl = import.meta.env.VITE_API_BASE
-
-
-export const fetchCharacters = async(page:number) =>  fetch(baseUrl + "character?page="+ page).then((res) => res.json())
-  
+export const fetchCharacters = async (page: number) =>
+  fetch(baseUrl + 'character?page=' + page).then((res) => res.json());
