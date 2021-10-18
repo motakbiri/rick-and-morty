cf2c9dd79fdaefc2bdd647641fbb6fca29f4cee0
Mohammad Takbiri <mo.takbiri@gmail.com>
2021-10-18 07:39:26 +0000

    refactor: change the url import for codesandbox

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -1,4 +1,5 @@
-const baseUrl = import.meta.env.VITE_API_BASE;
+const baseUrl =
+  import.meta.env.VITE_API_BASE || 'https://rickandmortyapi.com/api/';
 
 export const fetchCharacters = async (page: number) =>
   fetch(baseUrl + 'character?page=' + page).then((res) => res.json());
